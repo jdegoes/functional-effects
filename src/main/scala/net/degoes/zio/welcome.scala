886b59ac8825da2b51c682a6654443999681763b
John A. De Goes <john@degoes.net>
2020-06-11 02:54:12 -0600

    Update

---
+++ src/main/scala/net/degoes/zio/welcome.scala
@@ -0,0 +1,18 @@
+package net.degoes.zio
+
+object welcome {
+  //
+  // Please join and say hello:
+  //
+  // CHAT ROOM:  https://gitter.im/jdegoes/functional-scala
+  //
+  // Please git clone and build:
+  //
+  // REPOSITORY: https://github.com/jdegoes/zio-intro-game
+  //
+  // Daily Schedule:
+  //
+  //    START : 10:00 AM
+  //    LUNCH :  1:00 PM
+  //    END   :  6:30 PM
+}
