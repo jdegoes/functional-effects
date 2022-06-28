f47f1998bc691030233014e8ca212296521da512
John A. De Goes <john@degoes.net>
2022-06-27 22:30:55 -0400

    the rest

--- src/main/scala/net/degoes/zio/08-monitoring.scala
+++ src/main/scala/net/degoes/zio/08-monitoring.scala
@@ -1,1 +1,43 @@
 package net.degoes.zio
+
+import zio._
+
+object SimpleLogging extends ZIOAppDefault {
+
+  /**
+   * EXERCISE
+   *
+   * Add logging using `ZIO.log` around each update of the ref.
+   */
+  val program =
+    for {
+      ref   <- Ref.make(0)
+      _     <- ZIO.foreachPar(0 to 10)(i => ref.update(_ + i))
+      value <- ref.get
+    } yield value
+
+  /**
+   * EXERCISE
+   *
+   * Surround `program` in `LogLevel.Error` to change its log level.
+   */
+  val program2: ZIO[Any, Nothing, Int] = program
+
+  val run = program *> program2
+}
+
+object LogSpan extends ZIOAppDefault {
+
+  /**
+   * EXERCISE
+   *
+   * Add a log span of "createUser" to the whole function.
+   */
+  def createUser(userName: String, passHash: String, salt: Long): ZIO[Any, Nothing, Unit] =
+    for {
+      _ <- ZIO.log(s"Creating user ${userName}")
+    } yield ()
+
+  val run =
+    createUser("sherlockholmes", "jkdf67sf6", 21381L)
+}
---
+++ src/main/scala/net/degoes/zio/10-structures.scala
@@ -0,0 +1,1 @@
+package net.degoes.zio
